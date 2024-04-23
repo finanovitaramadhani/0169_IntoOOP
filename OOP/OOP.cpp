f41cb62c390a09d911fc7187d960c236ad798260
finanovita <finanovita479@gmail.com>
2024-04-23 11:13:10 +0700

    Memanggil class dan objek

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -39,8 +39,18 @@ class Matakuliah {
         }
 };
 
-int main()
-{
-    
+int main() {
+    Mahasiswa mhs;
+    Matakuliah mk;
+
+    mhs.nama = "Budi";
+    mhs.umur = 20;
+    mhs.jurusan = "Teknik Informatika";
+    mhs.output();
+
+    mk.input();
+    mk.output();
+
+    return 0;
 }
 
