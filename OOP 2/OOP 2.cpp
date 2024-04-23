f03e42d50066cebd86a9780acab49558cc5428c1
finanovita <finanovita479@gmail.com>
2024-04-23 11:41:55 +0700

    Membuat objek berupa prosedure display dari akses public

--- OOP 2/OOP 2.cpp
+++ OOP 2/OOP 2.cpp
@@ -23,6 +23,12 @@ class bangunDatar {
         float hitungluas() {
             return panjang * lebar;
         }
+
+        void display() {
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << lebar << endl;
+            cout << "Luasnya = " << hitungluas() << endl;
+        }
         
 };
 
