7022b963bb1580753edc2c79550ee2c165d5cc52
finanovita <finanovita479@gmail.com>
2024-04-23 11:01:04 +0700

    Membuat objek berupa prosedur input dari akses public

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -23,6 +23,14 @@ class Matakuliah {
         string namaMk;
         int sks;
     public:
+        void input() {
+            cout << "Kode MK: ";
+            cin >> kodeMk;
+            cout << "Nama MK: ";
+            cin >> namaMk;
+            cout << "SKS: ";
+            cin >> sks;
+        }
 
 
 };
